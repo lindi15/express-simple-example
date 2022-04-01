97cde8213ab8a3cc571b90ae6b5307284ea5d264
lindubuhle mathenjwa <lindubuhle.mathenjwa@dadevs.co.za>
2022-04-01 16:03:07 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require('cors')
+const app = express();
+const port = 4300;
+
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/',(req,res)=>{
+    res.status(200).send("hello world")
+})
+app.post('/',(req,res)=>{
+    
+    const {name,name2,message}=req.body;
+
+    //console.log(body);
+    res.status(200).send({msg:`${name} ${message} ${name2} very very much`});
+} )
+
+app.listen(port,()=>console.log(`server runningon port : ${port}`))
